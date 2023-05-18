889572221fa5f6898821de6326562ad48209dc95
Rayanne Melo Dias <102603196+rayannemd@users.noreply.github.com>
2023-05-18 10:51:46 -0300

    Adding files from learningpython

---
+++ learningpython/index.py
@@ -0,0 +1,34 @@
+#Integer
+
+age = 17
+players = 2
+teams = 5
+
+print(f"There are {players * teams} players with {age} years old.")
+
+
+#Float
+
+distance = 0.9
+price = 9.99
+
+print(f"I'm {distance} away from the market. I'm gonna buy a notebook for the price of ${price}")
+
+
+#String
+
+name = "Brandon"
+humor = "hungry"
+food = "apple"
+
+print(f"{name} is {humor}, he want to eat an {food}.")
+
+
+#Boolean
+
+running = input("Is the code running?")
+
+if running == "yes":
+    print(True)
+else: print (False)
+
