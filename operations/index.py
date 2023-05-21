63b8d0fa9c7f3c2da9721900787a6b58ba5f863b
Rayanne Melo Dias <102603196+rayannemd@users.noreply.github.com>
2023-05-20 23:10:45 -0300

    Adding files from operations

---
+++ operations/index.py
@@ -0,0 +1,23 @@
+import math
+
+#friends = 5
+ 
+#friends += 1 #addition
+#friends -= 1 #subtraction
+#friends /= 2 #division
+#friends *= 2 #multiply
+#friends **= 2 #square
+#remainder = friends % 2 #module
+
+x = math.pi
+y = -4
+z = 9
+
+result1 = round(x) #round number
+print(result1)
+result2 = abs(y) #shows the absolut result from 0 of the number
+print(result2)
+result3 = pow(4, 3) #pow a number
+print(result3)
+result4 = math.sqrt(y) #square root of a number
+
